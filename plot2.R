7d1071f6a8da775c8c1dbf7c12ad423851201cf0
bhoover10001 <bhoover@acteva.com>
2014-07-10 20:13:53 -0700

    finished plot3.R and generated plot3.png

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 plot2 <- function(file, dateFrom="2007-02-01", dateTo="2007-02-02") {
-  timeData <- read.table(file, header=T, sep=";", na.strings="?", nrows=70000)
+  timeData <- read.table(file, header=T, sep=";", na.strings="?")
   timeData[[2]] <- strptime(paste(timeData[[1]], " ", timeData[[2]]) , format="%d/%m/%Y %H:%M:%S")
   timeData[[1]] <- as.Date(timeData[[1]], format="%d/%m/%Y")
   timeData <- timeData[timeData$Date>= as.Date(dateFrom) & timeData$Date <= as.Date(dateTo), ]
