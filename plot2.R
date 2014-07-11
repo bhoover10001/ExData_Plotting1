bd4444558e6ed01101f1d690e3cddc1d2b86871c
bhoover10001 <bhoover@acteva.com>
2014-07-10 20:04:54 -0700

    Finished plot2.R and generated plot2.png

--- plot2.R
+++ plot2.R
@@ -1,8 +1,9 @@
 plot2 <- function(file, dateFrom="2007-02-01", dateTo="2007-02-02") {
-  timeData <- read.table(file, header=T, sep=";", na.strings="?")
+  timeData <- read.table(file, header=T, sep=";", na.strings="?", nrows=70000)
   timeData[[2]] <- strptime(paste(timeData[[1]], " ", timeData[[2]]) , format="%d/%m/%Y %H:%M:%S")
   timeData[[1]] <- as.Date(timeData[[1]], format="%d/%m/%Y")
   timeData <- timeData[timeData$Date>= as.Date(dateFrom) & timeData$Date <= as.Date(dateTo), ]
-  plot(timeData$Time, timeData$Global_active_power, xaxt="n")
-  axis.Date(side = 1, timeData$Date, format="%a")
+  plot(timeData$Time, timeData$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="n")
+  lines(timeData$Time, timeData$Global_active_power)
+  axis.Date(side = 1, timeData$Time, format="%a")
 }
